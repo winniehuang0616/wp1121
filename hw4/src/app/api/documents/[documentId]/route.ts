eed7e73fea135aa6c6ad2c114ecf70bc721c2678
winnie0616 <winniebear1213@gmail.com>
2023-12-17 16:53:33 +0800

    deploy check

--- hw4/src/app/api/documents/[documentId]/route.ts
+++ hw4/src/app/api/documents/[documentId]/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 
 import { db } from "@/db";
@@ -46,33 +45,3 @@ export async function POST(request: NextRequest) {
   return new NextResponse("OK", { status: 200 });
 }
 
-export async function DELETE(request: NextRequest) {
-  const data = await request.json();
-
-  try {
-    JoinRequestSchema.parse(data);
-  } catch (error) {
-    return NextResponse.json({ error: "Invalid request" }, { status: 400 });
-  }
-
-  const { handle, activityID } = data as JoinRequest;
-
-  try {
-    await db
-      .delete(joinsTable)
-      .where(
-        and(
-          eq(joinsTable.handle, handle),
-          eq(joinsTable.activityID, activityID),
-        ),
-      )
-      .execute();
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Something went wrong" },
-      { status: 500 },
-    );
-  }
-
-  return new NextResponse("OK", { status: 200 });
-}
